3d7e2073212b4702d2f096f99936862ec461ebec
Murray Smith <murraysmith2727@gmail.com>
2020-07-16 22:48:02 -0300

    Added javadocs for alertpack stuff

--- phase1/src/AlertPack/TradePastDateAlert.java
+++ phase1/src/AlertPack/TradePastDateAlert.java
@@ -3,12 +3,22 @@ package AlertPack;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+/**
+ * A user alert that is sent to a user when the meetup date for one of their trades has passed.
+ */
 public class TradePastDateAlert extends UserAlert implements Serializable {
 
-    protected LocalDateTime dueDate;
-    protected String username;
-    protected int tradeId;
+    private LocalDateTime dueDate;
+    private String username;
+    private int tradeId;
 
+    /**
+     * Constructs a new TradePastDateAlert, storing data about when the trade was due to happen and the other user
+     * involved.
+     * @param dueDate A LocalDateTimeObject
+     * @param username The username of the user.
+     * @param tradeId the unique ID of the trade.
+     */
     public TradePastDateAlert(LocalDateTime dueDate, String username, int tradeId){
         super(7);
         this.dueDate = dueDate;
--- phase1/src/AlertPack/TradeRequestCancelledAlert.java
+++ phase1/src/AlertPack/TradeRequestCancelledAlert.java
@@ -2,10 +2,18 @@ package AlertPack;
 
 import java.io.Serializable;
 
+/**
+ * A user alert that alerts a user when their trade request has been cancelled because one of the items in that request
+ * has been traded away to annother user.
+ */
 public class TradeRequestCancelledAlert extends UserAlert implements Serializable {
     //author: Louis Scheffer V in group 0110 for CSC207H1 summer 2020 project
     protected int tradeID;
 
+    /**
+     * Constructs a new TradeRequestCancelledAlert, storing a unique TradeID
+     * @param tradeID the unique ID of the trade that was cancelled.
+     */
     public TradeRequestCancelledAlert(int tradeID){
         super(9);
         this.tradeID = tradeID;
