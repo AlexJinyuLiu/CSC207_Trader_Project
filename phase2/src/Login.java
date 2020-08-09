265682e98678d1546e464fc4eb47d01796e18cc0
razdanri <riya.razdan@mail.utoronto.ca>
2020-08-09 16:57:18 -0400

    implemented login add new admin

--- phase2/src/Login.java
+++ phase2/src/Login.java
@@ -39,15 +39,18 @@ public class Login extends JFrame{
                     AdminActions adminActions = new AdminActions();
                     validLogin = adminActions.validateLogin(useCases.adminUser, user, pass);
                     if (validLogin) {
-                        //TODO send user to Options
                         //TODO send admin to adminActionsMenu instead of userActionsMenu
                         AdminActionsMenu adminOptionsMenu = new AdminActionsMenu(useCases,
                                 cpg, frame);
+                        cpg.adminAlertManager.handleAlertQueue(cpg.menuPresenter, useCases.adminUser,
+                                useCases.userManager, useCases.tradeCreator, useCases.itemManager,
+                                useCases.adminUser.getAdminAlerts());
                     } else {
                         JOptionPane.showMessageDialog(frame, cpg.menuPresenter.getText(Frame.LOGIN,4));
                     }
                 }else{
                     UserActions userActions = new UserActions();
+                    // TODO send user to Options
                     //This version of validateLogin returns null if the login is invalid, true if the user is a
                     //TradingUser, and false if the user is a BrowsingUser
                     validLogin = userActions.validateLogin(useCases.userManager, user, pass);
