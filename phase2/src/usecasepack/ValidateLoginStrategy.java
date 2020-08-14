7b2650c684d41b10b52b98b0ccec3874ac8bf7f2
Jinyu Liu <alexjinyu.liu@mail.utoronto.ca>
2020-08-14 04:07:20 -0400

    Javadocs for strategy pattern

--- phase2/src/usecasepack/ValidateLoginStrategy.java
+++ phase2/src/usecasepack/ValidateLoginStrategy.java
@@ -5,6 +5,13 @@ import java.util.HashMap;
 
 public class ValidateLoginStrategy implements Serializable {
 
+    /** Method that validates login
+     *
+     * @param username login username
+     * @param password login password
+     * @param validLogins a hashmap with valid usernames as keys and their passwords as values
+     * @return whether or not the login is valid
+     */
     public boolean validateLogin(String username, String password, HashMap<String, String> validLogins) {
         String login = null;
         for (String loginUsername : validLogins.keySet()) {
@@ -20,6 +27,12 @@ public class ValidateLoginStrategy implements Serializable {
         return false;
     }
 
+    /** Method that returns whether or not the username is available
+     *
+     * @param username username that is checked
+     * @param validLogins a hashmap with valid usernames as keys and their passwords as values
+     * @return true if the username is not a key in validLogins. false otherwise.
+     */
     public boolean usernameAvailable(String username, HashMap<String, String> validLogins) {
         if (validLogins.isEmpty()) {
             return true;
