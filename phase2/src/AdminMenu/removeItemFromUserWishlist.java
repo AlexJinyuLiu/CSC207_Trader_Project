2a37237cf0bf563b73fea9d4a8eacc20b6b3299a
yangmel3 <melwy.yang@mail.utoronto.ca>
2020-08-13 20:47:59 -0400

    unfinished removeItemFromUserWishlist

--- phase2/src/AdminMenu/removeItemFromUserWishlist.java
+++ phase2/src/AdminMenu/removeItemFromUserWishlist.java
@@ -7,15 +7,27 @@ import entitypack.TradingUser;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 //have already chosen user
 public class removeItemFromUserWishlist {
     private JPanel mainPanel;
     private JLabel menuTitle;
-    private JButton Submit;
-    private JScrollPane UserItems;
+    private JButton submit;
+    private JScrollPane userItems;
 public removeItemFromUserWishlist(UseCaseGrouper useCases, ControllerPresenterGrouper cpg, String username, JFrame window){
+    window.setContentPane(mainPanel);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.pack();
     TradingUser user = (TradingUser) useCases.userManager.searchUser(username);
+    menuTitle.setText(cpg.menuPresenter.getText(Frame.ADMINVIEWUSER, 2));
+    submit.setText(cpg.menuPresenter.getText(Frame.ADMINVIEWUSER, 6));
+    window.setVisible(true);
+
+    String[] items = user.getWishlistItemNames().toArray(new String[0]);
+    JCheckBox[] boxes = new JCheckBox();
+
+
 }
 
 }
